60f4a7cbc5d989c88f12636ef31ec024e1976750
ryan-doan <doanngocdung01@gmail.com>
2024-02-05 14:40:37 -0500

    add gitignore

--- Findaroo.Server/obj/Debug/net8.0/Findaroo.Server.AssemblyInfo.cs
+++ Findaroo.Server/obj/Debug/net8.0/Findaroo.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Findaroo.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae7fb1701834e74c931eee11bc1534fe1a6003b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbb575d7de0f36613e7ea8cdb45743d3587b78b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Findaroo.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Findaroo.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
