9540ad008e5b726e4d3256b4768f459c3fbf99e9
ryan-doan <doanngocdung01@gmail.com>
2024-02-08 00:28:48 -0500

    dependency injection for s3

--- Findaroo.Server/obj/Debug/net8.0/Findaroo.Server.AssemblyInfo.cs
+++ Findaroo.Server/obj/Debug/net8.0/Findaroo.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Findaroo.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5759f9ecc0174979c94742cd731f608b9550da4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+281d06b5e260865d818dbd54a5b47ad5f65d12b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Findaroo.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Findaroo.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
