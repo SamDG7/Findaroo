7e38463657a8cb03386a0a5803c940ca5d53c314
ryan-doan <doanngocdung01@gmail.com>
2024-02-10 17:11:59 -0500

    finish upload profile picture and implement get profile picture

--- Findaroo.Server/Controllers/ImageController.cs
+++ Findaroo.Server/Controllers/ImageController.cs
@@ -1,6 +1,7 @@
 ﻿using Findaroo.Server.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Net;
 
 namespace Findaroo.Server.Controllers
@@ -12,7 +13,40 @@ namespace Findaroo.Server.Controllers
         [HttpGet]
         public IFormFile getProfilePicture(String user_id)
         {
-            return null;
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "Images\\Profile", user_id);
+
+            try
+            {
+                if (!Directory.Exists(path)) 
+                {
+                    Response.StatusCode = ((int)HttpStatusCode.NotFound);
+                    return null;
+                }
+
+                var fileList = Directory.GetFiles(path);
+
+                if (fileList.Length == 0)
+                {
+                    Response.StatusCode = (int)HttpStatusCode.NotFound;
+                    return null;
+                }
+
+                path = fileList[0];
+
+                using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read))
+                {
+                    return new FormFile(stream, 0, stream.Length, "inline", Path.GetFileName(stream.Name))
+                    {
+                        Headers = new HeaderDictionary(),
+                        ContentType = "image/" + Path.GetExtension(stream.Name)
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                return null;
+            }
         }
 
         [HttpPost]
@@ -20,13 +54,21 @@ namespace Findaroo.Server.Controllers
         {
             try
             {
-                string path = Path.Combine(Directory.GetCurrentDirectory(), imageModel.image_name);
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "Images/Profile", imageModel.user_id);
+
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                
+                path = Path.Combine(path, imageModel.image_name);
 
                 using (Stream stream = new FileStream(path, FileMode.Create))
                 {
                     imageModel.form_file.CopyTo(stream);
                 }
-            } catch (Exception ex)
+            } 
+            catch (Exception ex)
             {
                 Response.StatusCode = ((int)HttpStatusCode.InternalServerError);
             }
--- Findaroo.Server/obj/Debug/net8.0/Findaroo.Server.AssemblyInfo.cs
+++ Findaroo.Server/obj/Debug/net8.0/Findaroo.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Findaroo.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+281d06b5e260865d818dbd54a5b47ad5f65d12b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9540ad008e5b726e4d3256b4768f459c3fbf99e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Findaroo.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Findaroo.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
