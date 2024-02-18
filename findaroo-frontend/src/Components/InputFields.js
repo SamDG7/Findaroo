6759894e57645dd0b770d59dbb5556090cca11a1
jhcheng01 <jhcheng@purdue.edu>
2024-02-18 16:19:10 -0500

    Show/hide password function

--- findaroo-frontend/src/Components/InputFields.js
+++ findaroo-frontend/src/Components/InputFields.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, {useState} from "react";
 import './InputFields.css';
+
+import {Icon} from 'react-icons-kit';
+import {eyeOff} from 'react-icons-kit/feather/eyeOff';
+import {eye} from 'react-icons-kit/feather/eye'
 
 export default function InputStandard({name, defaultValue, onChangeFunction}) {
     return (
@@ -7,20 +11,48 @@ export default function InputStandard({name, defaultValue, onChangeFunction}) {
             <h3>
                 {name}
             </h3>
-            <input className="InputStandard" name={name} value={defaultValue} onChange={onChangeFunction}
-                   placeholder="..."/>
+            <input
+                className="InputStandard"
+                name={name}
+                value={defaultValue}
+                onChange={onChangeFunction}
+                placeholder="..."
+            />
         </div>
     );
 }
 
 export function InputPassword({name, defaultValue, onChangeFunction}) {
+
+    const [showPassword, setShowPassword] = useState(false);
+    const [icon, setIcon] = useState(eyeOff);
+
+    const togglePasswordVisibility = () => {
+        if (!showPassword) {
+           setIcon(eye);
+           setShowPassword(true)
+        } else {
+           setIcon(eyeOff)
+           setShowPassword(false)
+        }
+     }
+
     return (
         <div className="Row">
             <h3>
                 {name}
             </h3>
-            <input className="InputStandard" name={name} type="password" value={defaultValue}
-                   onChange={onChangeFunction} placeholder="..."/>
+            <input 
+                className="InputStandard"
+                name={name}
+                type={showPassword ? "text" : "password"}
+                value={defaultValue}
+                onChange={onChangeFunction}
+                placeholder="..."
+            />
+            <span class="flex justify-around items-center" onClick={togglePasswordVisibility}>
+                <Icon class="absolute mr-10" icon={icon} size={25}/>
+            </span>
         </div>
     );
 }
