dd3b05611d14233104364b82a8f7058a48215c33
andyjsharpe <ajsusa1@gmail.com>
2024-02-21 17:29:17 -0500

    Search setup

---
+++ findaroo-frontend/src/Components/PersonInfo.js
@@ -0,0 +1,65 @@
+import React from "react";
+import './PersonInfo.css';
+
+export default function PersonInfo({personDict}) {
+    return (
+        <div className="Row Start">
+            <img className="ProfileImage" src="https://andysharpe.dev/wp-content/uploads/2024/02/MeGGJ.png"
+                 alt={personDict.first_name + " " + personDict.last_name + "'s profile picture"}/>
+            <div className="Column Start">
+                <h1>
+                    {personDict.first_name + " " + personDict.last_name}
+                </h1>
+                <h2>
+                    {(personDict.state ? personDict.state + ", " : "") + personDict.country}
+                </h2>
+                <h2>
+                    {(!(personDict.school === null || personDict.school === "")
+                        ? "Student" + " at " + personDict.school
+                        : "")
+                    }
+                </h2>
+                <h2>
+                    {(!(personDict.company === null || personDict.company === "")
+                        ? "Employee" + " at " + personDict.company
+                        : "")
+                    }
+                </h2>
+                // Interests
+            </div>
+            <h1 className="Column End">
+                {personDict.rating + "/5"}
+            </h1>
+        </div>
+    );
+}
+
+export function PersonInfoSmall({personDict}) {
+    return (
+        <div className="Row Start">
+            <img className="ProfileImageSmall" src="https://andysharpe.dev/wp-content/uploads/2024/02/MeGGJ.png"
+                 alt={personDict.first_name + " " + personDict.last_name + "'s profile picture"}/>
+            <div className="Column Start">
+                <h3>
+                    {personDict.first_name + " " + personDict.last_name}
+                </h3>
+                <h4>
+                    {(!(personDict.school === null || personDict.school === "")
+                        ? "Student" + " at " + personDict.school
+                        : "")
+                    }
+                </h4>
+                <h4>
+                    {(!(personDict.company === null || personDict.company === "")
+                        ? "Employee" + " at " + personDict.company
+                        : "")
+                    }
+                </h4>
+                // Interests
+            </div>
+            <h3 className="Column End">
+                {personDict.rating + "/5"}
+            </h3>
+        </div>
+    );
+}
--- findaroo-frontend/src/Pages/Search.js
+++ findaroo-frontend/src/Pages/Search.js
@@ -5,6 +5,7 @@ import {Link, useNavigate} from "react-router-dom";
 import {useEffect, useState} from "react";
 import InputStandard from "../Components/InputFields";
 import ButtonStandard, {ButtonImportant} from "../Components/Buttons";
+import {PersonInfoSmall} from "../Components/PersonInfo";
 
 export default function Search() {
     // This redirects to the login page if not logged in
@@ -16,17 +17,55 @@ export default function Search() {
         }
     }, []);
 
+    // TODO: For now this data is hardcoded, but this is what should be returned by the get call
+    const tempData = [{
+        user_id: "IDK",
+        first_name: "Andy",
+        last_name: "Sharpe",
+        email: "ajsusa1@gmail.com",
+        phone: "1234567890",
+        age: 22,
+        address: "1407 My Road",
+        state: "Indiana",
+        country: "USA",
+        zip_code: "22182",
+        occupation: "Student",
+        company: "Telos",
+        school: "Purdue University",
+        rating: 4.5,
+        date_created: "2024-02-13T19:09:02.274Z",
+        date_modified: "2024-02-13T19:09:02.274Z",
+        status: true
+    }, {
+        user_id: "IDK",
+        first_name: "Andy",
+        last_name: "Sharp",
+        email: "ajsusa2@gmail.com",
+        phone: "1234567890",
+        age: 22,
+        address: "1407 My Road",
+        state: "Indiana",
+        country: "USA",
+        zip_code: "22182",
+        occupation: "Student",
+        company: "Dovel",
+        school: "Indiana University",
+        rating: 4.5,
+        date_created: "2024-02-13T19:09:02.274Z",
+        date_modified: "2024-02-13T19:09:02.274Z",
+        status: true
+    }];
+
     return (
         <div className="Page">
             <Navbar/>
             <div className="Panel mx-[2vw] my-[2vh] px-[1vw] py-[1vh] drop-shadow-xl">
                 <div className="Grid">
-                    <h1>
-                        test
-                    </h1>
-                    <h1>
-                        test 2
-                    </h1>
+                    {
+                        tempData.map((person, index) => (
+                            <PersonInfoSmall personDict={person}/>
+                        ))
+                    }
                 </div>
             </div>
         </div>
