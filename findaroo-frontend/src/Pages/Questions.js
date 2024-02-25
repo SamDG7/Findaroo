b9c58d9f5bd885c2a6c26c5952771e762616224c
Sai Girap <saigirap@gmail.com>
2024-02-25 15:10:49 -0500

    tweaked questions page text

--- findaroo-frontend/src/Pages/Questions.js
+++ findaroo-frontend/src/Pages/Questions.js
@@ -134,8 +134,8 @@ export default function Questions() {
                 Lifestyle questions to ensure the best matches!
             </h1>
 
-            <Form>
-                <h3>I am currently:</h3>
+            <form className="space-y-4 w-1/2 mx-auto" style={{width: "90%", alignSelf: "center"}}>
+                <div className="text-lg font-semibold">I am currently:</div>
                 <input type="radio" id="student" name="status" value="0" className="radio-option" defaultChecked={status === 0} onChange={(e) => setStatus(e.target.value)} />
                 <label htmlFor="student">a Student</label>
 
@@ -145,121 +145,121 @@ export default function Questions() {
                 <input type="radio" id="other" name="status" value="2" className="radio-option" onChange={(e) => setStatus(e.target.value)} />
                 <label htmlFor="other">Other</label>
 
-                <h3>Do you want your roommate(s) to be the same?</h3>
+                <div className="text-lg font-semibold">Do you want your roommate(s) to be the same?</div>
                 <input type="radio" id="roommatePreferences" name="roommatePreferences" value="0" className="radio-option" defaultChecked={status === 0} onChange={(e) => setRoommatePreferences(e.target.value)} />
                 <label htmlFor="roommatePreferences">Yes</label>
 
                 <input type="radio" id="roommatePreferences" name="roommatePreferences" value="1" className="radio-option" defaultChecked={status === 1} onChange={(e) => setRoommatePreferences(e.target.value)} />
                 <label htmlFor="roommatePreferences">No</label>
 
-                <h3>Do you smoke/vape?</h3>
-                <select id="smoke" name="smoke" value={smoke} onChange={(e) => setSmoke(e.target.value)}>
+                <div className="text-lg font-semibold">Do you smoke/vape?</div>
+                <select id="smoke" name="smoke" className="form-select rounded-md border-gray-300 shadow-sm" value={smoke} onChange={(e) => setSmoke(e.target.value)}>
                     <option value="0">Never</option>
                     <option value="1">Occasionally</option>
                     <option value="2">Regularly</option>
                 </select>
 
-                <h3>Is it okay if your roommate is a smoker and/or vapes:</h3>
-                <select id="roommateSmoke" name="roommateSmoke" value={roommateSmoke} onChange={(e) => setRoommateSmoke(e.target.value)}>
+                <div className="text-lg font-semibold">Is it okay if your roommate is a smoker and/or vapes:</div>
+                <select id="roommateSmoke" name="roommateSmoke" className="form-select rounded-md border-gray-300 shadow-sm" value={roommateSmoke} onChange={(e) => setRoommateSmoke(e.target.value)}>
                     <option value="0">Yes</option>
                     <option value="1">No</option>
                     <option value="2">Indifferent</option>
                 </select>
 
-                <h3>How often do you have visitors (friends, family, relationships) over?</h3>
-                <select id="visitors" name="visitors" value={visitors} onChange={(e) => setVisitors(e.target.value)}>
+                <div className="text-lg font-semibold">How often do you have visitors (friends, family, relationships) over?</div>
+                <select id="visitors" name="visitors" className="form-select rounded-md border-gray-300 shadow-sm" value={visitors} onChange={(e) => setVisitors(e.target.value)}>
                     <option value="0">Never</option>
                     <option value="1">Sometimes</option>
                     <option value="2">Often</option>
                 </select>
 
-                <h3>Is it okay if your roommate has visitors over, and if so how often?</h3>
-                <select id="roommateVisitors" name="roommateVisitors" value={roommateVisitors} onChange={(e) => setRoommateVisitors(e.target.value)}>
+                <div className="text-lg font-semibold">Is it okay if your roommate has visitors over, and if so how often?</div>
+                <select id="roommateVisitors" name="roommateVisitors" className="form-select rounded-md border-gray-300 shadow-sm" value={roommateVisitors} onChange={(e) => setRoommateVisitors(e.target.value)}>
                     <option value="0">Never</option>
                     <option value="1">Sometimes</option>
                     <option value="2">Often</option>
                 </select>
 
-                <h3>Do you have any pets?</h3>
-                <select id="0" name="pets" value={pets} onChange={(e) => setPets(e.target.value)}>
+                <div className="text-lg font-semibold">Do you have any pets?</div>
+                <select id="0" name="pets" className="form-select rounded-md border-gray-300 shadow-sm" value={pets} onChange={(e) => setPets(e.target.value)}>
                     <option value="1">No</option>
                     <option value="2">Yes</option>
                 </select>
 
-                <h3>Is it okay if your roommate has pets?</h3>
-                <select id="roommatePets" name="roommatePets" value={roommatePets} onChange={(e) => setRoommatePets(e.target.value)}>
+                <div className="text-lg font-semibold">Is it okay if your roommate has pets?</div>
+                <select id="roommatePets" name="roommatePets" className="form-select rounded-md border-gray-300 shadow-sm" value={roommatePets} onChange={(e) => setRoommatePets(e.target.value)}>
                     <option value="0">Yes</option>
                     <option value="1">No</option>
                     <option value="2">Indifferent</option>
                 </select>
 
-                <h3>What is your primary focus?</h3>
-                <select id="focus" name="focus" value={focus} onChange={(e) => setFocus(e.target.value)}>
+                <div className="text-lg font-semibold">What is your primary focus?</div>
+                <select id="focus" name="focus" className="form-select rounded-md border-gray-300 shadow-sm" value={focus} onChange={(e) => setFocus(e.target.value)}>
                     <option value="0">Studies</option>
                     <option value="0">Work</option>
                     <option value="1">Social Life</option>
                     <option value="1">Hobbies</option>
                 </select>
 
-                <h3>How much time do you spend playing video games per week:</h3>
-                <select id="gamingTime" name="gamingTime" value={gamingTime} onChange={(e) => setGamingTime(e.target.value)}>
+                <div className="text-lg font-semibold">How much time do you spend playing video games per week:</div>
+                <select id="gamingTime" name="gamingTime" className="form-select rounded-md border-gray-300 shadow-sm" value={gamingTime} onChange={(e) => setGamingTime(e.target.value)}>
                     <option value="0">None</option>
                     <option value="1">Less than 5 hours</option>
                     <option value="2">5 to 10 hours</option>
                     <option value="3">More than 10 hours</option>
                 </select>
 
-                <h3>How tidy of a person are you:</h3>
-                <select id="tidiness" name="tidiness" value={tidiness} onChange={(e) => setTidiness(e.target.value)}>
+                <div className="text-lg font-semibold">How tidy of a person are you:</div>
+                <select id="tidiness" name="tidiness" className="form-select rounded-md border-gray-300 shadow-sm" value={tidiness} onChange={(e) => setTidiness(e.target.value)}>
                     <option value="0">Very Tidy</option>
                     <option value="1">Average</option>
                     <option value="2">Not Tidy</option>
                 </select>
 
-                <h3>How often do you clean your living space:</h3>
-                <select id="cleaningFrequency" name="cleaningFrequency" value={cleaningFrequency} onChange={(e) => setCleaningFrequency(e.target.value)}>
+                <div className="text-lg font-semibold">How often do you clean your living space:</div>
+                <select id="cleaningFrequency" name="cleaningFrequency" className="form-select rounded-md border-gray-300 shadow-sm" value={cleaningFrequency} onChange={(e) => setCleaningFrequency(e.target.value)}>
                     <option value="0">Daily</option>
                     <option value="1">Weekly</option>
                     <option value="2">Monthly</option>
                 </select>
 
-                <h3>How much does noise bother you while studying/working:</h3>
-                <select id="noiseSensitivity" name="noiseSensitivity" value={noiseSensitivity} onChange={(e) => setNoiseSensitivity(e.target.value)}>
+                <div className="text-lg font-semibold">How much does noise bother you while studying/working:</div>
+                <select id="noiseSensitivity" name="noiseSensitivity" className="form-select rounded-md border-gray-300 shadow-sm" value={noiseSensitivity} onChange={(e) => setNoiseSensitivity(e.target.value)}>
                     <option value="0">A lot</option>
                     <option value="1">A little</option>
                     <option value="2">Not at all</option>
                 </select>
 
-                <h3>How often do you go out to socialize:</h3>
-                <select id="socializingFrequency" name="socializingFrequency" value={socializingFrequency} onChange={(e) => setSocializingFrequency(e.target.value)}>
+                <div className="text-lg font-semibold">How often do you go out to socialize:</div>
+                <select id="socializingFrequency" name="socializingFrequency" className="form-select rounded-md border-gray-300 shadow-sm" value={socializingFrequency} onChange={(e) => setSocializingFrequency(e.target.value)}>
                     <option value="0">Rarely</option>
                     <option value="1">Occasionally</option>
                     <option value="2">Frequently</option>
                 </select>
 
-                <h3>How often do you drink alcohol:</h3>
-                <select id="alcoholConsumption" name="alcoholConsumption" value={alcoholConsumption} onChange={(e) => setAlcoholConsumption(e.target.value)}>
+                <div className="text-lg font-semibold">How often do you drink alcohol:</div>
+                <select id="alcoholConsumption" name="alcoholConsumption" className="form-select rounded-md border-gray-300 shadow-sm" value={alcoholConsumption} onChange={(e) => setAlcoholConsumption(e.target.value)}>
                     <option value="0">Never</option>
                     <option value="1">Occasionally</option>
                     <option value="2">Regularly</option>
                 </select>
 
-                <h3>How often do you use recreational drugs:</h3>
-                <select id="drugUse" name="drugUse" value={drugUse} onChange={(e) => setDrugUse(e.target.value)}>
+                <div className="text-lg font-semibold">How often do you use recreational drugs:</div>
+                <select id="drugUse" name="drugUse" className="form-select rounded-md border-gray-300 shadow-sm" value={drugUse} onChange={(e) => setDrugUse(e.target.value)}>
                     <option value="0">Never</option>
                     <option value="1">Occasionally</option>
                     <option value="2">Regularly</option>
                 </select>
 
-                <h3>How often do you borrow your friend's or sibling's personal items?</h3>
-                <select id="borrowingFrequency" name="borrowingFrequency" value={borrowingFrequency} onChange={(e) => setBorrowingFrequency(e.target.value)}>
+                <div className="text-lg font-semibold">How often do you borrow your friend's or sibling's personal items?</div>
+                <select id="borrowingFrequency" name="borrowingFrequency" className="form-select rounded-md border-gray-300 shadow-sm" value={borrowingFrequency} onChange={(e) => setBorrowingFrequency(e.target.value)}>
                     <option value="0">Never</option>
                     <option value="1">Occasionally</option>
                     <option value="2">Frequently</option>
                 </select>
 
                 <ButtonImportant text="Submit" onClickFunction={togglePopup} />
-            </Form>
+            </form>
             <p> </p>
         </div>
     );
