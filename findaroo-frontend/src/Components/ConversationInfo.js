3e57f9e689d1f9939985ba893aa905bd5bdd56f9
ryan-doan <doanngocdung01@gmail.com>
2024-04-09 16:07:57 -0400

    finish delete message frontend

--- findaroo-frontend/src/Components/ConversationInfo.js
+++ findaroo-frontend/src/Components/ConversationInfo.js
@@ -2,11 +2,20 @@ import React, { useEffect, useState } from "react";
 import './PersonInfo.css';
 import GlobalVariables from "../Utils/GlobalVariables";
 import { useNavigate } from "react-router-dom";
+import { FaRegTrashCan } from "react-icons/fa6";
+import { RxCross1 } from "react-icons/rx";
+import Popup from "./Popup";
 
 export function MessageStyle({messageInfo}){
     //console.log(messageInfo.date_modified)
-
+    const [isVisible, setVisible] = useState(true);
     const [userName, setUserName] = useState();
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+    const closePopup = () => {
+        setIsPopupOpen(false);
+    }
+
     const date = new Date(messageInfo.date_modified);
 
     useEffect(() => {
@@ -22,15 +31,39 @@ export function MessageStyle({messageInfo}){
     }, []);
 
     return(
+        isVisible ?
         <div
             className={"Column drop-shadow-xl my-[1.5vh]" + (messageInfo.user_id === GlobalVariables.userCredential.uid ? " End bg-blue-200 ml-auto" : " Start bg-gray-200  mr-auto")}>
+            <Popup isOpen={isPopupOpen} closePopup={closePopup}>
+                <h2>Delete Message</h2>
+                <p>Are you sure you want to delete this message?</p>
+                <div style={{ marginTop: '10px', display: 'flex', justifyContent: 'center', gap: '10px' }}>
+                    <button style={{ background: '#007AFF', color: 'white', border: 'none', padding: '10px 20px', cursor: 'pointer' }} onClick={deleteMessage}>Yes</button>
+                    <button style={{ background: '#808080', color: 'white', border: 'none', padding: '10px 20px', cursor: 'pointer' }} onClick={closePopup}>Cancel</button>
+                </div>
+            </Popup>
+            {messageInfo.user_id === GlobalVariables.userCredential.uid ? <RxCross1 onClick={() => setIsPopupOpen(true)}/> : <div></div>}
             <div className="Row">
                 <h3>{userName}</h3>
                 <h3>{date.toLocaleString()}</h3>
             </div>
             <h2>{messageInfo.message_text}</h2>
         </div>
+        :
+        <div></div>
     );
+
+    async function deleteMessage() {
+        await fetch(GlobalVariables.backendURL + "/Conversation/message", {
+            method: 'DELETE',
+            credentials: 'include',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({'message_id': messageInfo.message_id})
+        }).catch(error => console.log(error));
+        setVisible(false);
+    }
 }
 
 // Only show the most recent message and people in the conversation
