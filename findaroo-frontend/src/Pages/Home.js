c2701c37505c255647c7c2f7548f604df34f72af
andyjsharpe <ajsusa1@gmail.com>
2024-04-26 03:39:20 -0400

    In progress code

--- findaroo-frontend/src/Pages/Home.js
+++ findaroo-frontend/src/Pages/Home.js
@@ -4,17 +4,17 @@ import GlobalVariables from "../Utils/GlobalVariables";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Popup from "../Components/Popup";
+import {forEach} from "react-bootstrap/ElementChildren";
+import {PersonInfoSmall} from "../Components/PersonInfo";
+import {RoomAndRoommates} from "../Components/RoomAndRoommates";
 
 export default function Home() {
     // This redirects to the login page if not logged in
     const navigate = useNavigate();
-
     const [status, setStatus] = useState(true);
-    // const [isPopupOpen, setIsPopupOpen] = useState(false);
-
-    // const togglePopup = () => {
-    //     setIsPopupOpen(!isPopupOpen);
-    // };
+    const [userArr, setUserArr] = useState();
+    const [roomArr, setRoomArr] = useState();
+    const [combinedArr, setCombinedArr] = useState();
 
     useEffect(() => {
         if (!GlobalVariables.authenticated || GlobalVariables.userCredential.uid === undefined) {
@@ -35,49 +35,146 @@ export default function Home() {
             .catch(error => console.error(error));
     }, []);
 
-    
+    useEffect(() => {
+        fetch('http://localhost:5019/User/all')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setUserArr(data);
+            })
+            .catch(error => console.error(error));
+        fetch('http://localhost:5019/Room')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setRoomArr(data);
+            })
+            .catch(error => console.error(error));
+    }, [])
 
-    // const reactivateAccount = () => {
-    //     console.log("PUT Call")
-    //     try {
-    //         const form = {
-    //             user_id: GlobalVariables.userCredential.uid,
-    //             status: true
-    //         }
-    //         console.log(form);
-    //         fetch('http://localhost:5019/User', {
-    //             method: "PUT",
-    //             headers: {
-    //                 "Content-Type": "application/json",
-    //             },
-    //             body: JSON.stringify(form)
-    //         }).then(response => {
-    //             return response.text()
-    //         });
-    //     } catch (err) {
-    //         console.log(err)
-    //     } finally {
-    //         navigate("/Profile");
-    //     }
-    // }
+    useEffect(() => {
+        // this merges arrays
+        let newArr = [...roomArr, ...userArr];
+        const sortedArr = newArr.sort(FeaturedSort);
+        setCombinedArr(sortedArr);
+    }, [roomArr, userArr])
 
     return (
         <div className="Page">
             <Navbar />
-
-            {/* <Popup isOpen={!status} closePopup={togglePopup}>
-                <h2>Reactivate Account?</h2>
-                <p>Your account is currently deactivated. Would you like to reactivate it?</p>
-                <div style={{ marginTop: '10px', display: 'flex', justifyContent: 'center', gap: '10px' }}>
-                    <button style={{ background: '#007AFF', color: 'white', border: 'none', padding: '10px 20px', cursor: 'pointer' }} onClick={reactivateAccount}>Yes</button>
-                    <button style={{ background: '#808080', color: 'white', border: 'none', padding: '10px 20px', cursor: 'pointer' }} onClick={() => {GlobalVariables.authenticated = false; navigate("/Login");}}>Close Browser</button>
-                </div>
-            </Popup> */}
-
-            <h3>
-                This is the home page! Click on the buttons on the Navbar to move to a different page!
-            </h3>
+            <div className="Column">
+                combinedArr.map((data, index) => (
+                    data[status] != null
+                    ?
+                    <PersonInfoSmall key={index} personDict={data} similarityOutput={calculateLifestyleSimilarity(data)} />
+                    :
+                    room {false && <RoomAndRoommates key={index} roomDict={data} similarityOutput={calculateRoomLifestyleSimilarity(data)} />}
+                ))
+            </div>
         </div>
     );
+
+    function FeaturedSort(sortTypeName){
+        return function (a, b) { return calculateSimilarity(b) - calculateSimilarity(a) };
+    }
+
+    function calculateSimilarity(dataDict) {
+        if (dataDict[status] != null) {
+            return calculateRoomLifestyleSimilarity(dataDict);
+        } else {
+            return calculateRoomLifestyleSimilarity(dataDict);
+        }
+    }
+
+
+    function calculateRoomLifestyleSimilarity(roomDict) {
+        if (roomDict != null && roomDict.length > 0){
+        let totalSimilarity = 0;
+        roomDict.forEach(function (value.roommate_id, index, array) {
+                totalSimilarity += calculateLifestyleSimilarity()
+            }
+        );
+        totalSimilarity /= roomDict.length;
+        return totalSimilarity;
+        }
+    }
+
+    function calculateLifestyleSimilarity(personDict) {
+        if (userData != null) {
+            var questions = personDict.lifestyle_answers;
+            var userQuestions = userData.lifestyle_answers;
+
+            var lifestyle_sim = 0;
+
+            if (questions != null && userQuestions != null) {
+                lifestyle_sim += 10 - Math.abs(questions[0] - userQuestions[0]) * 5;
+
+                if (questions[1] == 1 || userQuestions[1] == 1) {
+                    if (questions[0] != userQuestions[0]) {
+                        lifestyle_sim *= 0;
+                    }
+                }
+
+                lifestyle_sim += 10 - Math.abs(questions[2] - userQuestions[2]) * 5;
+
+                if (questions[3] == 1 || userQuestions[3] == 1) {
+                    if (questions[2] != userQuestions[2]) {
+                        lifestyle_sim *= 0;
+                    }
+                }
+
+                lifestyle_sim += 10 - Math.abs(questions[4] - userQuestions[4]) * 5;
+
+                if (questions[5] == 1 || userQuestions[5] == 1) {
+                    if (questions[4] != userQuestions[4]) {
+                        lifestyle_sim *= 0;
+                    }
+                }
+
+                if (questions[6] == userQuestions[6]) {
+                    lifestyle_sim += 5;
+                }
+
+                if (questions[7] == 1) {
+                    if (userQuestions[6] == 0) {
+                        lifestyle_sim *= 0;
+                    }
+                }
+                if (userQuestions[7] == 1) {
+                    if (questions[6] == 0) {
+                        lifestyle_sim *= 0;
+                    }
+                }
+
+                if (questions[8] == userQuestions[8]) {
+                    lifestyle_sim += 5;
+                }
+
+                lifestyle_sim += 10 - Math.abs(questions[9] - userQuestions[9]) * 3.33;
+
+                lifestyle_sim += 20 - Math.abs(questions[10] - userQuestions[10]) * 10;
+
+                lifestyle_sim += 10 - Math.abs(questions[11] - userQuestions[11]) * 5;
+
+                lifestyle_sim += 20 - Math.abs(questions[12] - userQuestions[12]) * 10;
+
+                lifestyle_sim += 10 - Math.abs(questions[13] - userQuestions[13]) * 5;
+
+                lifestyle_sim += 20 - Math.abs(questions[14] - userQuestions[14]) * 10;
+
+                lifestyle_sim += 20 - Math.abs(questions[15] - userQuestions[15]) * 10;
+
+                lifestyle_sim += 20 - Math.abs(questions[16] - userQuestions[16]) * 10;
+
+            }
+        }
+
+        lifestyle_sim *= (100 / 170);
+        lifestyle_sim = Math.round(lifestyle_sim * 100) / 100;
+
+        console.log("Lifestyle Sim between " + personDict.first_name + " and " + userData.first_name + ": " + lifestyle_sim);
+
+        return lifestyle_sim;
+    }
 
 }
